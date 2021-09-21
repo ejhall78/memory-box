dc0a02186d4eba66a56aad1b442e907aff04b40a
Madeleine Hughes <maddierosecodes@github.com>
2021-09-21 16:05:59 +0100

    Extracts authentication into databse fire (firebase.js)

--- src/routes/user.svelte
+++ src/routes/user.svelte
@@ -1,35 +1,8 @@
 <script>
     import { goto } from '$app/navigation';
-    import { initializeApp } from '@firebase/app';
-    import { getFirestore } from '@firebase/firestore/lite';
     import { getAuth, onAuthStateChanged } from 'firebase/auth';
-    import { getUserInfo } from '../lib/firebase.js';
-    const apiKey = import.meta.env.VITE_KEY;
-    const authDomain = import.meta.env.VITE_AUTHDOMAIN;
-    const projectId = import.meta.env.VITE_PROJECTID;
-    const storageBucket = import.meta.env.VITE_STORAGEBUCKET;
-    const messagingSenderId = import.meta.env.VITE_MESSAGINGSENDERID;
-    const appId = import.meta.env.VITE_APPID;
-    const measurementId = import.meta.env.VITE_MEASUREMENTID;
-
-    // Follow this pattern to import other Firebase services
-    // import { } from 'firebase/<service>';
-
-    const firebaseConfig = {
-        apiKey,
-        authDomain,
-        projectId,
-        storageBucket,
-        messagingSenderId,
-        appId,
-        measurementId,
-    };
-
-    export const app = initializeApp(firebaseConfig);
-    export const db = getFirestore(app);
-    const auth = getAuth();
-    // console.log(auth);
-
+    import { getUserInfo, db, auth } from '../lib/firebase.js';
+   
     export const logout = () => {
         auth.signOut();
         goto('/signin');
