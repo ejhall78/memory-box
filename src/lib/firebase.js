6dce17a06c36b2ef35f769e18c0fe9c45a46ae14
ed <ejhall78@gmail.com>
2021-09-22 22:33:54 +0100

    fix multiple answers per day bug

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,18 +1,17 @@
-import { initializeApp } from "@firebase/app";
-import { getAuth } from "@firebase/auth";
+import { initializeApp } from '@firebase/app';
+import { getAuth } from '@firebase/auth';
 import {
   getFirestore,
   collection,
   getDocs,
   doc,
   getDoc,
-
   updateDoc,
   arrayRemove,
   arrayUnion,
   query,
-  where
-} from "@firebase/firestore/lite";
+  where,
+} from '@firebase/firestore/lite';
 
 const apiKey = import.meta.env.VITE_KEY;
 const authDomain = import.meta.env.VITE_AUTHDOMAIN;
@@ -44,26 +43,24 @@ export const auth = getAuth();
 
 // Get a list of users from your database
 export const getUsers = async () => {
-  const usersCol = collection(db, "users");
+  const usersCol = collection(db, 'users');
   const userSnapshot = await getDocs(usersCol);
-  const userList = userSnapshot.docs.map((doc) => doc.data());
+  const userList = userSnapshot.docs.map(doc => doc.data());
   return userList;
 };
 
-export const getUserInfo = async (uid) => {
-  const docRef = doc(db, "users", uid);
+export const getUserInfo = async uid => {
+  const docRef = doc(db, 'users', uid);
   const docSnap = await getDoc(docRef);
   const docData = docSnap.data();
   return docData;
 };
 
-
-
-export const getAnswers = async (uid) => {
+export const getAnswers = async uid => {
   const docRef = doc(db, 'answers', uid);
   const docSnap = await getDoc(docRef);
   const docData = docSnap.data();
-  return docData
+  return docData;
 };
 
 export const getQuestions = async () => {
@@ -73,12 +70,15 @@ export const getQuestions = async () => {
   return docData;
 };
 
-export const getTodaysQuestion = async (day) => {
+export const getTodaysQuestion = async day => {
   const docRef = doc(db, 'questions', 'question_sets');
   const docSnap = await getDoc(docRef);
   const docData = docSnap.data();
-  const todaysQuestion = docData.original_set.filter(question => question['question_id'] === day);
+  const todaysQuestion = docData.original_set.filter(
+    question => question['question_id'] === day
+  );
   return todaysQuestion[0];
+};
 
 export const getAnswersByUser = async uid => {
   const docRef = doc(db, 'answers', uid);
@@ -87,7 +87,7 @@ export const getAnswersByUser = async uid => {
   return answers.original_set;
 };
 
-export const createAnsDaysRefObj = async (uid) => {
+export const createAnsDaysRefObj = async uid => {
   // check if a user is signed in
   // await get answers by user
   const answers = await getAnswersByUser(uid);
@@ -99,9 +99,9 @@ export const createAnsDaysRefObj = async (uid) => {
   // check each answers date
   // if date of answer already exists in formattedAnswers, push answer to that date array
   // else create an array with that dates key and push answer to that array
-  answers.forEach((answer) => {
+  answers.forEach(answer => {
     if (answer.date in formattedAnswers) {
-      answer.date.push(answer);
+      formattedAnswers[answer.date].push(answer);
     } else {
       formattedAnswers[answer.date] = [answer];
     }
@@ -112,14 +112,14 @@ export const createAnsDaysRefObj = async (uid) => {
 };
 
 export const deleteMemory = async (answer, uid) => {
-  const memoryRef = doc(db, "answers", uid);
+  const memoryRef = doc(db, 'answers', uid);
   await updateDoc(memoryRef, {
     original_set: arrayRemove(answer),
   });
 };
 
 export const toggleMemoryForget = async (answers, answer, uid, i) => {
-  const memoryRef = doc(db, "answers", uid);
+  const memoryRef = doc(db, 'answers', uid);
   const newAnswer = { ...answer };
   newAnswer.forget ? (newAnswer.forget = false) : (newAnswer.forget = true);
   const newArray = [...answers];
@@ -127,4 +127,9 @@ export const toggleMemoryForget = async (answers, answer, uid, i) => {
   await updateDoc(memoryRef, {
     original_set: newArray,
   });
+};
+
+export const updateUserInfo = async (userInfo, uid) => {
+  const userRef = doc(db, 'users', uid);
+  await updateDoc(userRef, userInfo);
 };
